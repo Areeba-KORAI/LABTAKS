cc386cb140785f5543c54ab3591395597d53d383
ak-23-AI <areebaabbasskorai@gmail.com>
2026-09-23 22:56:25 +0500

    Update IOT TASK 2.py

--- IOT TASK 2.py
+++ IOT TASK 2.py
@@ -1,8 +1,8 @@
-name = input("Enter student's name: ")
+Aname = input("Enter student's name: ")
 
 print("Uppercase:", name.upper())
 print("Lowercase:", name.lower())
 print("Number of characters:", len(name))
 if name:
   print("First character:", name[0])
-  print("Last character:", name[-1])
+  print("Last character:", name[-1])A
